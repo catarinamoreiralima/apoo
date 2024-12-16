5a00ed90af40a9cc621f47fcdeb659987c54d5b1
Pietra <73311085+pijuma@users.noreply.github.com>
2024-12-16 13:02:52 -0300

    Update models.py

--- agendamento_psicologico/usuarios/models.py
+++ agendamento_psicologico/usuarios/models.py
@@ -8,6 +8,7 @@ from enum import Enum
 from django.contrib.auth.hashers import make_password
 
 class Usuario(models.Model):
+    #informações que queremos salvar sobre o usuário no banco de dados
     nome = models.CharField(max_length=100)
     email = models.EmailField(max_length=254)
     telefone = models.CharField(max_length=15)
@@ -22,18 +23,19 @@ class Usuario(models.Model):
     def set_password(self, raw_password):
         self.senha = make_password(raw_password)
         self.save()
-        
+
+#campos adicionais para pacientes, que são herdados da classe usuário
 class Paciente (Usuario):
     data_nascimento = models.DateField()
     numero_usp = models.CharField(max_length=10)
-    
+
+#campos adicionais para psicólogos, que são herdados da classe usuário
 class Psicologo(Usuario):
     registro_profissional = models.CharField(max_length=100)
     abordagem = models.CharField(max_length=100)
 
 class Administrador (Usuario):
     cargo = models.CharField(max_length=100) 
-
 
     @classmethod
     def choices(cls):
@@ -43,7 +45,8 @@ class Hora(Enum):
     @classmethod
     def choices(cls):
         return [(f'{hora:02}h00', f'{hora}:00') for hora in range(24)]
-
+#classe de horários que utilizaremos para salvar no banco de dados, temos que são atrelados sempre a um psicólogo 
+#e quando temos uma consulta marcada ele passa a ter um paciente associado também 
 class Horario(models.Model):
     psicologo = models.ForeignKey(
         Psicologo, on_delete=models.CASCADE, related_name="horarios"
@@ -61,7 +64,7 @@ class Horario(models.Model):
     paciente = models.ForeignKey(
         Paciente, null=True, blank=True, on_delete=models.SET_NULL, related_name="horarios"
     )
-    
+    #operação de marcar consulta e alterações necessárias no banco de dados
     def marcar_consulta(self, paciente):
         if self.disponivel:
             self.paciente = paciente
